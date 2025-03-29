d50624dd9abc4c42c536051be58e9fd6a2069a04
twpro8 <twpro8@gmail.com>
2025-03-29 03:30:54 +0300

    Add universal methods to base repository / Add get_db_user to users repository / Add DBUserDataMapper to mappers

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,5 +1,10 @@
-from pydantic import BaseModel
+from select import select
 
+from pydantic import BaseModel
+from sqlalchemy import insert
+from sqlalchemy.exc import NoResultFound
+
+from src.exceptions import ObjectNotFoundException
 from src.repositories.mappers.base import DataMapper
 
 
@@ -10,3 +15,30 @@ class BaseRepository:
 
     def __init__(self, session):
         self.session = session
+
+    async def get_one(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        res = await self.session.execute(query)
+        try:
+            model = res.scalars().one()
+        except NoResultFound:
+            raise ObjectNotFoundException
+        return self.mapper.map_to_domain_entity(model)
+
+    async def get_one_or_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        res = await self.session.execute(query)
+        model = res.scalars().one_or_none()
+        if model is None:
+            return None
+        return self.mapper.map_to_domain_entity(model)
+
+    async def add_one(self, data: BaseModel) -> BaseModel:
+        stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        res = await self.session.execute(stmt)
+        model = res.scalars().one()
+        return self.mapper.map_to_domain_entity(model)
+
+    async def add_bulk(self, data: list[BaseModel]):
+        stmt = insert(self.model).values([item.model_dump() for item in data])
+        await self.session.execute(stmt)
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,5 +1,9 @@
+from sqlalchemy import select
+from sqlalchemy.exc import NoResultFound
+
+from src.exceptions import ObjectNotFoundException
 from src.repositories.base import BaseRepository
-from src.repositories.mappers.mappers import UserDataMapper
+from src.repositories.mappers.mappers import UserDataMapper, DBUserDataMapper
 
 from src.models.users import UserORM
 from src.schemas.users import UserDTO
@@ -9,3 +13,12 @@ class UsersRepository(BaseRepository):
     model = UserORM
     schema = UserDTO
     mapper = UserDataMapper
+
+    async def get_db_user(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        res = await self.session.execute(query)
+        try:
+            model = res.scalar_one()
+        except NoResultFound:
+            raise ObjectNotFoundException
+        return DBUserDataMapper.map_to_domain_entity(model)
