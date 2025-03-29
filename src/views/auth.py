1c62b83bac1e730a73c267564bfbc00f852af82c
twpro8 <twpro8@gmail.com>
2025-03-29 03:38:06 +0300

    Implementation of endpoints for login and registration

--- src/views/auth.py
+++ src/views/auth.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
+from src.schemas.users import UserAddRequestDTO, UserLoginRequestDTO
+from src.services.users import UserService
 from src.views.dependencies import DBDep
 
 
@@ -7,8 +9,13 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post("/register")
-async def register(db: DBDep): ...
+async def register(db: DBDep, user_data: UserAddRequestDTO):
+    user = await UserService(db).register_user(user_data)
+    return {"status": "ok", "user": user}
 
 
 @router.post("/login")
-async def login_with_password(db: DBDep): ...
+async def login_with_password(db: DBDep, user_data: UserLoginRequestDTO, response: Response):
+    access_token = await UserService(db).login_with_password(user_data)
+    response.set_cookie("access_token", access_token, httponly=True)
+    return {"status": "success", "access_token": access_token}
