f1d33587004eebeddce1f13d888cab473d1aab38
twpro8 <twpro8@gmail.com>
2025-03-31 06:36:33 +0300

    Add FavoritesORM / Run alembic migration

--- services/users-service/src/models/__init__.py
+++ services/users-service/src/models/__init__.py
@@ -1,6 +1,4 @@
 from src.models.users import UserORM, user_friend_association
+from src.models.favorites import FavoritesORM
 
-__all__ = [
-    "UserORM",
-    "user_friend_association",
-]
+__all__ = ["UserORM", "user_friend_association", "FavoritesORM"]
