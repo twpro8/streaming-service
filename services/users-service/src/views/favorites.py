88595d8f2008b6da660b10008d9d5c13bcbbfe29
twpro8 <twpro8@gmail.com>
2025-04-02 18:49:32 +0300

    Check if film exists when add to favorites fix: when user have nothing in favorites get all handle exceptions

--- services/users-service/src/views/favorites.py
+++ services/users-service/src/views/favorites.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter
 
+from src.exceptions import (
+    FilmNotFoundException,
+    FilmNotFoundHTTPException,
+    AlreadyInFavoritesHTTPException,
+    AlreadyInFavoritesException,
+)
 from src.views.dependencies import DBDep, UserIdDep, ACDep
 from src.services.favorites import FavoriteService
 
@@ -8,12 +14,17 @@ router = APIRouter(prefix="/favorites", tags=["Favorites"])
 
 
 @router.get("")
-async def get_favorites(db: DBDep, ac: ACDep, user_id: UserIdDep):
+async def get_my_favorites(db: DBDep, ac: ACDep, user_id: UserIdDep):
     favorites = await FavoriteService(db, ac).get_favorites(user_id=user_id)
     return {"status": "success", "favorites": favorites}
 
 
 @router.post("/{film_id}")
-async def add_favorite(db: DBDep, user_id: UserIdDep, film_id: int):
-    favorite = await FavoriteService(db).add_to_favorites(user_id=user_id, film_id=film_id)
+async def add_favorite(db: DBDep, ac: ACDep, user_id: UserIdDep, film_id: int):
+    try:
+        favorite = await FavoriteService(db, ac).add_to_favorites(user_id=user_id, film_id=film_id)
+    except FilmNotFoundException:
+        raise FilmNotFoundHTTPException
+    except AlreadyInFavoritesException:
+        raise AlreadyInFavoritesHTTPException
     return {"status": "success", "favorite": favorite}
