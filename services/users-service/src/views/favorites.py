6a982f8301c6e20ac6bf692a65cb7218a12525b3
twpro8 <twpro8@gmail.com>
2025-04-05 02:45:00 +0300

    Add get_ids to favorites repo Delete AsyncClient Dep

--- services/users-service/src/views/favorites.py
+++ services/users-service/src/views/favorites.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from src.adapters.service import ServiceAdapter
+from src.config import settings
 from src.exceptions import (
     FilmNotFoundException,
     FilmNotFoundHTTPException,
@@ -7,23 +9,24 @@ from src.exceptions import (
     AlreadyInFavoritesException,
 )
 from src.schemas.favorites import FavoriteAddRequestDTO
-from src.views.dependencies import DBDep, UserIdDep, ACDep
+from src.views.dependencies import DBDep, UserIdDep
 from src.services.favorites import FavoriteService
 
 
 router = APIRouter(prefix="/favorites", tags=["Favorites"])
+adapter = ServiceAdapter(content_service_url=settings.CONTENT_SERVICE_URL)
 
 
 @router.get("", summary="Get my favorites")
-async def get_favorites(db: DBDep, ac: ACDep, user_id: UserIdDep):
-    favorites = await FavoriteService(db, ac).get_favorites(user_id=user_id)
+async def get_favorites(db: DBDep, user_id: UserIdDep):
+    favorites = await FavoriteService(db, adapter).get_favorites(user_id=user_id)
     return {"status": "success", "favorites": favorites}
 
 
 @router.post("/{film_id}", summary="Add to favorites")
-async def add_favorite(db: DBDep, ac: ACDep, user_id: UserIdDep, favorite: FavoriteAddRequestDTO):
+async def add_favorite(db: DBDep, user_id: UserIdDep, favorite: FavoriteAddRequestDTO):
     try:
-        favorite = await FavoriteService(db, ac).add_to_favorites(user_id, favorite)
+        favorite = await FavoriteService(db, adapter).add_to_favorites(user_id, favorite)
     except FilmNotFoundException:
         raise FilmNotFoundHTTPException
     except AlreadyInFavoritesException:
