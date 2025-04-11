84b604a4a4b328b1ca8a5548885d4003d402aa35
twpro8 <twpro8@gmail.com>
2025-04-11 17:14:44 +0300

    Add Repo and DataMapper for seasons

--- services/content-service/src/repositories/mappers/mappers.py
+++ services/content-service/src/repositories/mappers/mappers.py
@@ -1,7 +1,8 @@
-from src.models import SeriesORM
+from src.models import SeriesORM, SeasonORM
 from src.repositories.mappers.base import DataMapper
 from src.models.films import FilmORM
 from src.schemas.films import FilmDTO
+from src.schemas.seasons import SeasonDTO
 from src.schemas.series import SeriesDTO
 
 
@@ -13,3 +14,8 @@ class FilmDataMapper(DataMapper):
 class SeriesDataMapper(DataMapper):
     db_model = SeriesORM
     schema = SeriesDTO
+
+
+class SeasonDataMapper(DataMapper):
+    db_model = SeasonORM
+    schema = SeasonDTO
