20c98fa0a5b8da230a0b6ab60d9f4f31b89c9b65
twpro8 <twpro8@gmail.com>
2025-04-29 17:50:07 +0300

    Add PlaylistORM and PlatlistItemORM Run alembic migrations

--- services/users-service/src/models/__init__.py
+++ services/users-service/src/models/__init__.py
@@ -1,4 +1,5 @@
 from src.models.users import UserORM, FriendshipORM
 from src.models.favorites import FavoritesORM
+from src.models.playlists import PlaylistORM, PlaylistItemORM
 
-__all__ = ["UserORM", "FriendshipORM", "FavoritesORM"]
+__all__ = ["UserORM", "FriendshipORM", "FavoritesORM", "PlaylistORM", "PlaylistItemORM"]
